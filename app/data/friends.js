73fdd7667ec324591c73697ca7772a314faab159
alex dawson <alexanderdawson@hotmail.ca>
2018-08-25 20:20:13 -0400

    commit

---
+++ app/data/friends.js
@@ -0,0 +1,83 @@
+
+
+var friendsList = [
+    {
+        "name":"Ahmed",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      },
+      {
+        "name":"John",
+        "scores":[
+            3,
+            2,
+            3,
+            4,
+            5,
+            1,
+            2,
+            3,
+            1,
+            1
+          ]
+      },
+      {
+        "name":"Mike",
+        "scores":[
+            2,
+            3,
+            4,
+            1,
+            4,
+            3,
+            2,
+            5,
+            2,
+            1
+          ]
+      },
+      {
+        "name":"Dave",
+        "scores":[
+            1,
+            5,
+            2,
+            2,
+            1,
+            5,
+            4,
+            1,
+            2,
+            5
+          ]
+      },
+      
+]
+
+module.exports = friendsList
+
+// function compare(alex, friend){
+//     var 
+//     for(i=0;i<5;i++){
+//         c.push(Math.abs(alex.scores[i]-friend.scores[i]))
+//     }
+//     score = sum(c)
+// }
+
+// function sum(arr){
+//     var s = 0
+//     for(i=0;i<arr.length;i++){
+//         s = s+arr[i];
+//     }
+//     return s;
+// }
---
+++ app/routes/apiRoutes.js
@@ -0,0 +1,26 @@
+
+var friends = require("../data/friends");
+
+
+
+module.exports = function(app) {
+
+
+  app.get("/api/friends", function(req, res) {
+    res.json(friends);
+  });
+
+
+  app.post("/api/friends", function(req, res) {
+   
+      friends.push(req.body);
+      res.json(true);
+    
+  });
+  app.post("/api/clear", function() {
+    friends= [];
+   
+    console.log(friends);
+  });
+
+};
